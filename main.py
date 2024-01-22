493ef4a15be507f9cdee1a58a27ff8cb7ebb79c4
APolomoshnova <nastyaaleks7@gmail.com>
2024-01-22 21:00:14 +0700

    многопоточность

--- main.py
+++ main.py
@@ -6,18 +6,14 @@ locker = threading.Lock()
 
 
 def analysis_text(text):
-    # словарь для хранения частот символов
     frequency = {}
-    # Подсчитываем общее число символов в тексте
     count_symbols = len(text)
-    # Проходимся по каждому символу в тексте
     for symbol in text:
         if symbol in frequency:
             frequency[symbol] += 1
         else:
             frequency[symbol] = 1
     time.sleep(1)
-    # Преобразуем частоты символов в пары (символ, частота встречаемости)
     result = [(symbol, frequency / count_symbols) for symbol, frequency in frequency.items()]
     return result
 
